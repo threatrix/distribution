aa65a1c902bb43ff5cb0548c7d3bd894a38e1d2a
John McDonald <john@threatrix.io>
2022-10-15 13:36:15 -0500

    Update threatagent-docker.sh

--- threatagent-docker.sh
+++ threatagent-docker.sh
@@ -1,6 +1,5 @@
 # get host environment variable to file
-printenv | grep CODEBUILD host.env >> host.env
-printenv | grep CODEBUILD host.env >> host.env
+printenv > host.env
 
 # add prefix to protect docker environment variables from being overwritten by host variables
 sed -i 's/^/HOST_/' host.env
@@ -8,7 +7,7 @@ sed -i 's/^/HOST_/' host.env
 cat host.env
 
 # run docker image
-#docker run --env-file=host.env test test_sh
+docker run --env-file=host.env test test_sh
 
 # deleting the temporary file
 rm host.env

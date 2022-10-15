98908f26d22648a358db0191e944db27d4a5e0d0
John McDonald <john@threatrix.io>
2022-10-15 16:34:40 -0500

    Update threatagent-docker.sh

--- threatagent-docker.sh
+++ threatagent-docker.sh
@@ -11,14 +11,4 @@ printenv | grep 'CIRCLE_' >> host.env
 # GitLab
 printenv | grep 'CI_' >> host.env
 
-
-# add prefix to protect docker environment variables from being overwritten by host variables
-#sed -i 's/^/HOST_/' host.env
-
-cat host.env
-
-# run docker image
-#docker run --env-file=host.env --name threatrix-threat-agent -v $(pwd):/app threatrix/threat-agent --oid=$HOST_THREATRIX_OID --eid=$HOST_THREATRIX_EID --api-key=$HOST_THREATRIX_API_KEY ./
-
-# deleting the temporary file
-#rm host.env
+#cat host.env

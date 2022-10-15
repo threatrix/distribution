7da1ce09a230315abfe6c1e9a5066fde59c22922
John McDonald <john@threatrix.io>
2022-10-15 14:56:33 -0500

    Update threatagent-docker.sh

--- threatagent-docker.sh
+++ threatagent-docker.sh
@@ -7,7 +7,7 @@ sed -i 's/^/HOST_/' host.env
 cat host.env
 
 # run docker image
-#docker run --env-file=host.env --name threatrix-threat-agent -v $(pwd):/app threatrix/threat-agent ./
+#docker run --env-file=host.env --name threatrix-threat-agent -v $(pwd):/app threatrix/threat-agent --oid=$HOST_THREATRIX_OID --eid=$HOST_THREATRIX_EID --api-key=$HOST_THREATRIX_API_KEY ./
 
 # deleting the temporary file
 #rm host.env

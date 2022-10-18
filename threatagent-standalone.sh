daf2f2f03089facdbceb0d8602a34f02273685aa
John McDonald <john@threatrix.io>
2022-10-17 19:04:02 -0500

    Update threatagent-standalone.sh

--- threatagent-standalone.sh
+++ threatagent-standalone.sh
@@ -12,9 +12,14 @@ if [ -f "mix.exs" ]; then
         if ! command -v mix &> /dev/null
         then
                 echo "[warning] mix may not be installed."
-                #exit
         fi
         mix archive.install hex sbom --force
+fi
+if [ -f "package.json" ]; then
+        if ! command -v npm &> /dev/null
+        then
+                echo "[warning] npm may not be installed."
+        fi
 fi
 echo "Downloading Threatrix Threat Agent.."
 curl -SsL https://github.com/threatrix/threat-matrix/releases/download/v1.2.9-RELEASE/threat-agent-1.2.9.6.jar -o threat-agent.jar
